695275a9ecdf9909fe574c57041ce15ce834ba93
Emmily <valeryano4ka@gmail.com>
2018-10-22 21:28:26 +0300

    Create Hobby-log (maven)

---
+++ src/test/java/Homework/Test.java
@@ -0,0 +1,27 @@
+package Homework;
+
+import org.apache.log4j.Logger;
+
+public class Test {
+
+    // Инициализация логера
+    private static final Logger log = Logger.getLogger(Test.class);
+
+    public static void main(String[] args) throws Exception {
+        Hobby person1 = new Hobby (false, 5, 3.5);
+        Hobby person2 = new Hobby (59, 15.9, '4',
+                3459, 0.60f);
+        Hobby person3 = new Hobby();
+        Hobby person4 = new Hobby(true, 84, 32.6,
+                (byte) 27, '5', 863,
+                (short) 53, 0.75f);
+        System.out.println(person1.toString());
+        System.out.println(person2.toString());
+        System.out.println(person3.toString());
+        System.out.println(person4.toString());
+
+        log.info("Это информационное сообщение!");
+        log.error("Это сообщение ошибки");
+        log.debug("Debug");
+    }
+}
